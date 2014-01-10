65578cd1151d4e5c933be70eac08687a9d525fc4
Eemu Bertling <eemu@bertling.fi>
2014-01-10 12:57:00 +0200

    Logging components added. Testcases.

---
+++ metka/src/main/java/fi/uta/fsd/metka/messaging/Logger.java
@@ -0,0 +1,10 @@
+package fi.uta.fsd.metka.messaging;
+
+/**
+ * Company: Proactum Oy
+ * User: Eemu Bertling
+ * Date: 18.12.2013
+ * Time: 14:31
+ */
+public class Logger {
+}
---
+++ metka/src/test/java/fi/uta/fsd/metka/service/LoggerTest.java
@@ -0,0 +1,10 @@
+package fi.uta.fsd.metka.service;
+
+/**
+ * Company: Proactum Oy
+ * User: Eemu Bertling
+ * Date: 19.12.2013
+ * Time: 14:38
+ */
+public class LoggerTest {
+}
