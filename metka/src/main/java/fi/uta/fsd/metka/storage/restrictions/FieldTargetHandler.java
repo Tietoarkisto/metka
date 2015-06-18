173d3bf544f91a564daa2e1e65817edb86750a6f
Lasse Kulmala <lasse.kulmala@proactum.fi>
2015-06-18 09:15:31 +0300

    Added copyright notice to code files.

--- metka/src/main/java/fi/uta/fsd/metka/storage/restrictions/FieldTargetHandler.java
+++ metka/src/main/java/fi/uta/fsd/metka/storage/restrictions/FieldTargetHandler.java
@@ -1,3 +1,31 @@
+/**************************************************************************************
+ * Copyright (c) 2013-2015, Finnish Social Science Data Archive/University of Tampere *
+ *                                                                                    *
+ * All rights reserved.                                                               *
+ *                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification,   *
+ * are permitted provided that the following conditions are met:                      *
+ * 1. Redistributions of source code must retain the above copyright notice, this     *
+ *    list of conditions and the following disclaimer.                                *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,       *
+ *    this list of conditions and the following disclaimer in the documentation       *
+ *    and/or other materials provided with the distribution.                          *
+ * 3. Neither the name of the copyright holder nor the names of its contributors      *
+ *    may be used to endorse or promote products derived from this software           *
+ *    without specific prior written permission.                                      *
+ *                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND    *
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED      *
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE             *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR   *
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES     *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;       *
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON     *
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT            *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                       *
+ **************************************************************************************/
+
 package fi.uta.fsd.metka.storage.restrictions;
 
 import fi.uta.fsd.Logger;
@@ -413,7 +441,7 @@ class FieldTargetHandler {
 
     // TODO: At the moment functions only for default language. This and the whole language handling within restrictions needs reworking
     // TODO: Add support for FIELD targets that are values, i.e. read target from configuration.
-    // TODO: Target needs to be able to travel in context so that both references and fields in parent can be checked. The target traveling needs to be diversified away from just prevalidation.
+    // TODO: Target needs to be able to travel in context so that both references and fields in parent can be checked. The target traveling needs to be diversified away from just pre-validation.
     private static class RegexCheck {
         private static boolean regex(ValueDataField field, Target t) {
             if(t == null || t.getType() != TargetType.VALUE || !StringUtils.hasText(t.getContent())) {
