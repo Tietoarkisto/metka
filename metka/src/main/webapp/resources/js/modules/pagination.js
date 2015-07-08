6b7820447535dd8e3124a9b4a12ec624eca89b35
Reko Peltola <reko.peltola@visma.com>
2015-07-08 13:54:36 +0300

    initial working paging
    metka #362 Taulukoille paginointi jotta hakutuloksia ja muita pitkiä listoja voi esittää paremmin

---
+++ metka/src/main/webapp/resources/js/modules/pagination.js
@@ -0,0 +1,66 @@
+/**************************************************************************************
+ * Copyright (c) 2013-2015, Finnish Social Science Data Archive/University of Tampere *
+ *                                                                                    *
+ * All rights reserved.                                                               *
+ *                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification,   *
+ * are permitted provided that the following conditions are met:                      *
+ * 1. Redistributions of source code must retain the above copyright notice, this     *
+ *    list of conditions and the following disclaimer.                                *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,       *
+ *    this list of conditions and the following disclaimer in the documentation       *
+ *    and/or other materials provided with the distribution.                          *
+ * 3. Neither the name of the copyright holder nor the names of its contributors      *
+ *    may be used to endorse or promote products derived from this software           *
+ *    without specific prior written permission.                                      *
+ *                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND    *
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED      *
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE             *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR   *
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES     *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;       *
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON     *
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT            *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                       *
+ **************************************************************************************/
+
+define(function (require) {
+    'use strict';
+
+    return function (options) {
+        var $paging;
+        if (options.field.rowsPerPage != null) {
+            $paging = $('<div>');
+            //it doesn't matter how many pages we show initially as we almost immediately
+            //trigger redraw within containerField.js with correct rows and perPage values
+            $paging.bootpag({
+                total: 5,
+                page: 1,
+                maxVisible: 5,
+                leaps: true,
+                firstLastUse: true,
+                first: '←',
+                last: '→'
+            }).on("page", function (event, num) {
+                //this event is triggered when we change pages and this triggers
+                //redraw on containerField.js
+                var redraw = 'redraw-{key}'.supplant({
+                    key: options.field.key
+                });
+                options.$events.trigger(redraw, [num]);
+            });
+
+            var redrawPaging = 'redraw-{key}-paging'.supplant({
+                key: options.field.key
+            });
+            //event to trigger paging redraw/recalc
+            options.$events.on(redrawPaging, function (event, perPage, rows) {
+                $paging.bootpag({total: Math.ceil(rows / perPage)});
+            });
+        }
+
+        return $paging;
+    };
+});
