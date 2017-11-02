4520c547898320a7dddefa8a37b00f6f600b2a50
Juuso Korhonen <juuso.korhonen@visma.com>
2017-11-02 15:47:28 +0200

    Aineiston ja sarjan nimet sivujen headereihin #469

--- metka/src/main/webapp/resources/js/modules/pages/defaults.js
+++ metka/src/main/webapp/resources/js/modules/pages/defaults.js
@@ -85,7 +85,7 @@ define(function (require) {
 
                     var header = {
                         localized: 'type.{page}.title',
-                        pattern: '{localized} - {id} - {no}{state}{handler}',
+                        pattern: '{localized} - {id}{name} - {no}{state}{handler}',
                         buttons: $buttons
                             .append($('<div class="btn-group btn-group-xs">')
                                 .append([{
@@ -151,10 +151,16 @@ define(function (require) {
                         value: options.data.key.no
                     });
 
+                    var title = options.data.fields['title'] !== undefined ?
+                        (options.data.fields['title'].values['DEFAULT'].current || options.data.fields['title'].values['DEFAULT'].original) : options.data.fields['seriesname'] !== undefined ?
+                            (options.data.fields['seriesname'].values['DEFAULT'].current || options.data.fields['seriesname'].values['DEFAULT'].original) : "";
+
+                    supplant.name = title ? ' - ' + title : '';
+
                     supplant.state = data.data.state.uiState ? ' - ' + MetkaJS.L10N.get('state.' + data.data.state.uiState) : '';
                     supplant.handler = data.data.state.handler ? ' - ' + MetkaJS.L10N.get('general.handler') + " " + data.data.state.handler : (data.data.state.uiState === 'DRAFT' ? ' - ' + MetkaJS.L10N.get('general.noHandler') : '');
                     supplant.localized = MetkaJS.L10N.get(header.localized.supplant(supplant));
-                    $header.html(header.pattern.supplant(supplant));
+                    $header.html("<div class='page-header-text'>" + header.pattern.supplant(supplant) + "</div>");
 
                     if (header.buttons) {
                         $header.append(header.buttons);
