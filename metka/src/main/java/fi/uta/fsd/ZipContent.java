fd48610ffb2722daa642277ee3f1cc50688d8c05
samuhynynen <samu.hynynen@gmail.com>
2018-10-26 14:40:34 +0300

    Edellisestä commitista unohtunut Java-luokka sekä issue-taulukko #401

---
+++ metka/src/main/java/fi/uta/fsd/ZipContent.java
@@ -0,0 +1,70 @@
+/**************************************************************************************
+ * Copyright (c) 2013-2015, Finnish Social Science Data Archive/University of Tampere *
+ *                                                                                    *
+ * All rights reserved.                                                               *
+ *                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification,   *
+ * are permitted provided that the following conditions are met:                      *
+ * 1. Redistributions of source code must retain the above copyright notice, this     *
+ *    list of conditions and the following disclaimer.                                *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,       *
+ *    this list of conditions and the following disclaimer in the documentation       *
+ *    and/or other materials provided with the distribution.                          *
+ * 3. Neither the name of the copyright holder nor the names of its contributors      *
+ *    may be used to endorse or promote products derived from this software           *
+ *    without specific prior written permission.                                      *
+ *                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND    *
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED      *
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE             *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR   *
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES     *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;       *
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON     *
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT            *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS      *
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                       *
+ **************************************************************************************/
+
+package fi.uta.fsd;
+
+import fi.uta.fsd.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public final class ZipContent {
+
+
+    /**
+     * Create and return a complete list of zip file contents with their paths.
+     *
+     * @param path path to zip file
+     * @return list of contents
+     */
+
+    public List <String> contentList(String path) {
+
+        List <String> list =  new ArrayList<>();
+
+        try {
+            ZipFile zipFile = new ZipFile(path);
+            Enumeration<? extends ZipEntry> e = zipFile.entries();
+
+            while (e.hasMoreElements()) {
+                ZipEntry entry = (ZipEntry) e.nextElement();
+                list.add(entry.getName());
+            }
+        }
+        catch(IOException ioe)
+        {
+            Logger.error(getClass(), "The zip file can't be opened "+ioe);
+        }
+        return list;
+    }
+}
