95e418333150822f23d2a82b819627f2502f3fc3
timoselin <timo.selin@vismaconsulting.fi>
2018-11-07 12:44:18 +0200

    Issue #581

--- metka/src/main/webapp/resources/js/modules/inputField.js
+++ metka/src/main/webapp/resources/js/modules/inputField.js
@@ -37,6 +37,9 @@ define(function (require) {
         if (key === 'relatedstudies_select' || key === 'studies_select' || (key === 'studyid' && options.fieldOptions.type === 'SELECTION') || (key === 'study' && options.fieldOptions.type === 'SELECTION')) {
             id = 'METKA_UI_999';
         }
+        if(options.field.key === 'othermateriallabel') {
+            options.field.multiline = true;
+        }
         var $label = require('./label')(options, lang)
             .attr('for', id);
 
